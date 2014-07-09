6095b31464b3d484a9faa5bd22c7fb20142c196f
Aaron Weitekamp <aweiteka@gmail.com>
2014-07-09 11:06:33 -0400

    fix server run.sh syntax errors

--- server/run.sh
+++ server/run.sh
@@ -4,14 +4,13 @@ case $1 in
   worker)
     if [ -n "$2" ]; then
       exec runuser apache \
-	-s /bin/bash /bin/bash \
+	-s /bin/bash \
 	-c "/usr/bin/celery worker \
 	--events --app=pulp.server.async.app \
 	--loglevel=INFO \
 	-c 1 \
 	-n reserved_resource_worker-$2@localhost \
-	--logfile=/var/log/pulp/reserved_resource_worker-$2.log \
-	--pidfile=/var/run/pulp/reserved_resource_worker-$2.pid"
+	--logfile=/var/log/pulp/reserved_resource_worker-$2.log"
     else
       echo "The 'worker' role must be assigned a unique number as the second positional argument."
       echo "For example 'worker 1'."
@@ -20,22 +19,15 @@ case $1 in
     fi
     ;;
   beat)
-    exec runuser apache \
-      -s /bin/bash /bin/bash \
-      -c "/usr/bin/celery beat 
-         --scheduler=pulp.server.async.scheduler.Scheduler \
-         --workdir=/var/lib/pulp/celery/ \
-         -f /var/log/pulp/celerybeat.log -l INFO \
-         --detach --pidfile=/var/run/pulp/celerybeat.pid"
+    exec runuser apache -s /bin/bash -c "/usr/bin/celery beat --workdir=/var/lib/pulp/celery --scheduler=pulp.server.async.scheduler.Scheduler -f /var/log/pulp/celerybeat.log -l INFO"
     ;;
   resource_manager)
     exec runuser apache \
-      -s /bin/bash /bin/bash \
+      -s /bin/bash \
       -c "/usr/bin/celery worker -c 1 -n resource_manager@localhost \
           --events --app=pulp.server.async.app \
           --loglevel=INFO -Q resource_manager \
-          --logfile=/var/log/pulp/resource_manager.log \
-          --pidfile=/var/run/pulp/resource_manager.pid"
+          --logfile=/var/log/pulp/resource_manager.log"
     ;;
   *)
     echo "'$1' is not a supported celery command."
