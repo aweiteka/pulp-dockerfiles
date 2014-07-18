2c181bc3bf508b5e40bfefe8a39f1817d8c05ee8
Aaron Weitekamp <aweiteka@gmail.com>
2014-07-18 15:04:25 -0400

    add ssl certs during runtime, add publish util container, use correct hostname in config data

--- config-data/run.sh
+++ config-data/run.sh
@@ -2,4 +2,5 @@
 
 # Modify /etc/pulp/server.conf with variables from 'docker run'
 cp /etc/pulp/server.conf{,.orig}
-sed -i "s/# seeds: localhost:27017/seeds: $MONGO:27017/" /etc/pulp/server.conf
+sed -i "s/# seeds: localhost:27017/seeds: $MONGO_HOST:27017/" /etc/pulp/server.conf
+sed -i "s/# server_name: server_hostname/server_name: $PULP_HOST/" /etc/pulp/server.conf
