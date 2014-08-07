731c0cae676eb0c9566de6f158e36e61fe948ddc
aweiteka <aweiteka@redhat.com>
2014-08-07 09:14:18 -0400

    installers

---
+++ centos/install_client.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "USAGE: `basename $0` <pulp_hostname>"
+  exit 1
+fi
+
+PULP_HOSTNAME=$1
+
+echo "Pulling docker images"
+
+IMAGES=( "aweiteka/pulp-admin" \
+         "aweiteka/pulp-publish-docker" )
+for i in "${IMAGES[@]}"; do sudo docker pull $i; done
+
+echo "Setting up ~/.pulp directory"
+
+mkdir ~/.pulp
+chcon -Rvt svirt_sandbox_file_t ~/.pulp
+
+cat << EOF > ~/.pulp/admin.conf
+[server]
+host = $PULP_HOSTNAME
+EOF
+
+cat << EOF > ~/.pulp/publish.conf
+[redirect]
+url=http://$PULP_HOSTNAME
+EOF
+
+ls -l ~/.pulp
+
+echo "Create /tmp/docker_uploads"
+sudo mkdir -p /tmp/docker_uploads
+sudo chcon -Rvt svirt_sandbox_file_t /tmp/docker_uploads
+
+echo "Update ~/.bashrc with aliases"
+echo "alias pulp-admin='sudo docker run --rm -t -v ~/.pulp:/.pulp -v /tmp/docker_uploads/:/tmp/docker_uploads/ aweiteka/pulp-admin'" >> ~/.bashrc
+echo "alias pulp-publish-docker='sudo docker run --rm -i -t -v ~/.pulp:/.pulp -v /tmp/docker_uploads/:/tmp/docker_uploads/ aweiteka/pulp-publish-docker'" >> ~/.bashrc
+source ~/.bashrc
+
+echo "2 aliases created"
+
+echo "Login with command \"pulp-admin login -u admin -p admin\""
---
+++ centos/install_server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "USAGE: `basename $0` <pulp_ip_address>"
+  exit 1
+fi
+
+PULP_IP=$1
+
+echo "Pulling docker images. This will take several minutes."
+
+IMAGES=( "aweiteka/pulp-crane-allinone" \
+         "aweiteka/pulp-worker" \
+         "aweiteka/pulp-qpid" \
+         "aweiteka/pulp-mongodb" \
+         "aweiteka/pulp-apache" \
+         "aweiteka/pulp-data" \
+         "aweiteka/pulp-centosbase" )
+for i in "${IMAGES[@]}"; do sudo docker pull $i; done
+
+echo "running docker images"
+
+# data
+sudo docker run -e PULP_HOST=$(hostname) -e MONGO_HOST=$PULP_IP -e QPID_HOST=$PULP_IP --name pulp-data aweiteka/pulp-data
+
+# mongo
+sudo docker run -d -p 27017:27017 --name pulp-mongodb aweiteka/pulp-mongodb
+
+# qpid
+sudo docker run -d -p 5672:5672 --name pulp-qpid aweiteka/pulp-qpid
+
+# apache -- creates/migrates pulp_database 
+sudo docker run -d --privileged -v /dev/log:/dev/log --volumes-from pulp-data -p 443:443 -p 8080:80 -e APACHE_HOSTNAME=$(hostname) --name pulp-apache aweiteka/pulp-apache
+
+# pulp workers
+sudo docker run -d -e WORKER_HOST=$PULP_IP --privileged -v /dev/log:/dev/log --volumes-from pulp-data --name pulp-worker1 aweiteka/pulp-worker worker 1
+sudo docker run -d -e WORKER_HOST=$PULP_IP --privileged -v /dev/log:/dev/log --volumes-from pulp-data --name pulp-worker2 aweiteka/pulp-worker worker 2
+sudo docker run -d --privileged -v /dev/log:/dev/log --volumes-from pulp-data --name pulp-beat aweiteka/pulp-worker beat
+sudo docker run -d -e WORKER_HOST=$PULP_IP --privileged -v /dev/log:/dev/log --volumes-from pulp-data --name pulp-resource_manager aweiteka/pulp-worker resource_manager
+
+# crane
+sudo docker run -d -p 80:80 --volumes-from pulp-data --name pulp-crane aweiteka/pulp-crane-allinone
