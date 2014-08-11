d33163f26d6540aa8a2c39f8bb9aee3f7dd5120b
aweiteka <aweiteka@redhat.com>
2014-08-11 11:24:14 -0400

    create build script

--- centos/build.sh
+++ centos/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-sudo docker build --no-cache -t aweiteka/pulp-centosbase base
-sudo docker build --no-cache -t aweiteka/pulp-admin admin-client
-sudo docker build --no-cache -t aweiteka/pulp-apache apache
-sudo docker build --no-cache -t aweiteka/pulp-crane-allinone crane-allinone
-sudo docker build --no-cache -t aweiteka/pulp-mongodb mongodb
-sudo docker build --no-cache -t aweiteka/pulp-data config-data
-sudo docker build --no-cache -t aweiteka/pulp-qpid qpid
-sudo docker build --no-cache -t aweiteka/pulp-worker worker
+declare -A IMAGES
 
+# ["build_directory"]=image/name
+IMAGES+=( ["base"]=aweiteka/pulp-centosbase \
+         ["config-data"]=aweiteka/pulp-data \
+         ["crane-allinone"]=aweiteka/pulp-crane-allinone \
+         ["worker"]=aweiteka/pulp-worker \
+         ["qpid"]=aweiteka/pulp-qpid \
+         ["mondodb"]=aweiteka/pulp-mongodb \
+         ["apache"]=aweiteka/pulp-apache \
+         ["admin-client"]=aweiteka/pulp-admin \
+         ["publish-client"]=aweiteka/pulp-publish-docker )
+
+for dir in ${!IMAGES[@]}; do
+  echo "Building ${IMAGES[${dir}]} ..."
+  sudo docker build --no-cache -t ${IMAGES[${dir}]} ${dir}
+done
